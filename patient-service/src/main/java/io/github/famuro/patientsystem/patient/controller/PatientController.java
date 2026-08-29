bafc49e678f0cc2d9ca5dc635ae71ef6338b8f7f
famuro <famuro.tech@proton.me>
2026-08-29 01:20:47 -0500

    feat(patient): add patient creation
    Handle POST requests for creating patients and persist the submitted
    patient data through the service and repository layers.

--- patient-service/src/main/java/io/github/famuro/patientsystem/patient/controller/PatientController.java
+++ patient-service/src/main/java/io/github/famuro/patientsystem/patient/controller/PatientController.java
@@ -1,9 +1,14 @@
 package io.github.famuro.patientsystem.patient.controller;
 
+import io.github.famuro.patientsystem.patient.dto.PatientRequestDTO;
 import io.github.famuro.patientsystem.patient.dto.PatientResponseDTO;
 import io.github.famuro.patientsystem.patient.service.PatientService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +28,12 @@ public class PatientController {
         List<PatientResponseDTO> patients = patientService.getPatients();
 
         return ResponseEntity.ok().body(patients);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(patientResponseDTO);
     }
 }
--- patient-service/src/main/java/io/github/famuro/patientsystem/patient/service/PatientService.java
+++ patient-service/src/main/java/io/github/famuro/patientsystem/patient/service/PatientService.java
@@ -1,17 +1,17 @@
 package io.github.famuro.patientsystem.patient.service;
 
+import io.github.famuro.patientsystem.patient.dto.PatientRequestDTO;
 import io.github.famuro.patientsystem.patient.dto.PatientResponseDTO;
 import io.github.famuro.patientsystem.patient.mapper.PatientMapper;
 import io.github.famuro.patientsystem.patient.model.Patient;
 import io.github.famuro.patientsystem.patient.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PatientService {
-    private PatientRepository patientRepository;
+    private final PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
@@ -19,13 +19,12 @@ public class PatientService {
 
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
-        return patients
-                .stream()
-                .map(PatientMapper::toDTO)
-                .toList();
+        return patients.stream().map(PatientMapper::toDTO).toList();
     }
 
-    public Patient save(Patient patient) {
-        return patientRepository.save(patient);
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+
+        return PatientMapper.toDTO(newPatient);
     }
 }
